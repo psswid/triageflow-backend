88364ae2ca51d91f26fb890fda1ad190ddf07cb7
Piotr Świderski <psswiderski@gmail.com>
2026-05-29 14:49:41 +0200

    test: add PHPUnit test infrastructure

---
+++ tests/SmokeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class SmokeTest extends TestCase
+{
+    public function testInfrastructureWorks(): void
+    {
+        $this->assertTrue(true);
+    }
+}
